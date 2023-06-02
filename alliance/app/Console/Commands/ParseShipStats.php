cde489303b3994896cd0c4c4ed26c77968454d5e
John Cashmore <JohnCashmore@users.noreply.github.com>
2023-06-02 13:00:30 +0100

    Update ParseShipStats.php

--- alliance/app/Console/Commands/ParseShipStats.php
+++ alliance/app/Console/Commands/ParseShipStats.php
@@ -41,7 +41,14 @@ class ParseShipStats extends Command
 	{
 		$url = Config::get('stats.ships');
 
-		$html = file_get_contents($url);
+		$context = stream_context_create(
+			array(
+				"http" => array(
+					"header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+				)
+			)
+		);
+		$html = file_get_contents($url, false, $context);
 
 		$ships = simplexml_load_string($html);
 
